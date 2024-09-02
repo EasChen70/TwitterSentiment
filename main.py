3409a055375972db0ac419aa506fa43ec19f389e
EasChen70 <eason_chen@uri.edu>
2024-09-01 20:13:55 -0400

    bookmark

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import os
 from configparser import ConfigParser
 from random import randint
 
+MINIMUM_TWEET = 10
+QUERY = 'chatgpt'
+
+
 load_dotenv(dotenv_path="secret.env")
 USERNAME = os.getenv('Username')
 EMAIL = os.getenv('Email')
@@ -15,13 +19,27 @@ PASSWORD = os.getenv('Password')
 
 # Initialize Client
 client = Client('en-US')
-
+    #Login
 async def main():
     await client.login(
         auth_info_1=USERNAME,
         auth_info_2=EMAIL,
         password=PASSWORD
     )
+    #Use Cookies   
+    await client.save_cookies('cookies.json')
 
+    client.load_cookies('cookies.json')
+
+    tweet_count = 0
+
+    tweets = await client.search_tweet(QUERY, product='Top')
+
+    for tweet in tweets:
+        tweet_count += 1
+        tweet_data = [tweet_count, tweet.user.name, tweet.text, tweet.created_at, tweet.retweet_count, tweet.favorite_count]
+        print(vars(tweet))
+        break
+
+# Run the main function
 asyncio.run(main())
-
